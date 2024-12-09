b4a29b4f24e600070c4a787294a02708060d7ce4
Sachin Giri <sachinkumargiri0909@gmail.com>
2024-12-09 15:25:11 +0530

    Update fetch.js

--- frontend/src/fetch/fetch.js
+++ frontend/src/fetch/fetch.js
@@ -1,5 +1,5 @@
 
-const backend_domain='https://easybuy-t2ot.onrender.com/api'
+const backend_domain='http://localhost:3000/api'
 const fetch_api={
       signup:{
         url:`${backend_domain}/signup`,
