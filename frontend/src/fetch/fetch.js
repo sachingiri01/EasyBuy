39729ecaed3638ce16483235a215219f2c3d13bf
Sachin Giri <sachinkumargiri0909@gmail.com>
2025-05-20 13:19:40 +0530

    Updated backend API

--- frontend/src/fetch/fetch.js
+++ frontend/src/fetch/fetch.js
@@ -1,4 +1,4 @@
-const backend_domain='http://localhost:3000/api'
+const backend_domain='https://easy-buy-back-ap.vercel.app/'
 const fetch_api={
       signup:{
         url:`${backend_domain}/signup`,
